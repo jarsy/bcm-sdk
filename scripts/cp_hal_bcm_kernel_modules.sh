0d3717a0348bb99aa500f3f1a06a871ec62ab4cc
Yoram Shechori <yshechori@mrv.com>
2017-07-23 13:27:40 +0300

    Autodetect x86 artifacts folder

--- scripts/cp_hal_bcm_kernel_modules.sh
+++ scripts/cp_hal_bcm_kernel_modules.sh
@@ -8,6 +8,12 @@ if [ ! -d $to_dir_deploy ] ; then
     mkdir $to_dir_deploy
 fi
 
-from_dir=$bcm_sdk_dir/systems/linux/user/x86-smp_generic-2_6
+from_dir=`find $bcm_sdk_dir/systems/linux/user/ -type d -name "x86-smp_generic-*"`
+if [ -z $from_dir ] ; then
+	echo "Couldn't locate artifacts source folder"
+	exit 1
+fi;
+
+#from_dir=$bcm_sdk_dir/systems/linux/user/x86-smp_generic-2_6
 cp $from_dir/linux-kernel-bde.ko $to_dir_deploy
 cp $from_dir/linux-user-bde.ko   $to_dir_deploy
