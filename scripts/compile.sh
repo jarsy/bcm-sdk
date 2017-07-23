a037f5f74f3018a3791fc022b176c32d038bce3d
Yoram Shechori <yshechori@mrv.com>
2017-07-23 12:00:54 +0300

    Update scripts/compile.sh

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+. $BCM_SDK/scripts/env_export_sdk-6.5.7_lk-4.9.env
 
 BCM_SDK=$1
 COMPILE_DIR=$BCM_SDK/systems/linux/user/x86-smp_generic-4_9
@@ -6,20 +7,11 @@ COMPILE_DIR=$BCM_SDK/systems/linux/user/x86-smp_generic-4_9
 ################################################################################
 # Check script user-supplied parameters
 #
-if [ -z $BCM_SDK ] ; then
+if [ -z "$BCM_SDK" ] ; then
 	echo "$0: Error: Provide BCM SDK directory!"
 	echo "$0: Example: $0 ~/My/MRV/OPX/BCM_SDK/Dev/git/bcm_sdk /tmp/bcm_sdk_artifacts"
 	exit 1;
 fi
-
-export ARCH=x86
-export TARGET=linux-kernel-4_9
-export TOOLCHAIN_DIR=$HOME/clovis_2015/buildtools/i686-nptl-linux-gnu
-export PATH=/usr/bin:$TOOLCHAIN_DIR/bin:$PATH
-export CROSS_COMPILE=i686-nptl-linux-gnu-
-export LDFLAGS="-L $TOOLCHAIN_DIR/lib"
-export OPT_CFLAGS=" -m32 -DMRV_STANDALONE"
-export MODULE_LDFLAGS=" -m elf_i386"
 
 cd $COMPILE_DIR
 if [ -s /opt/incredibuild/bin/ib_console ] && [ -n "$IB_CORES" ];
