0b19bd13fb1427035d6690a1e936bc504d498bac
Yoram Shechori <yshechori@mrv.com>
2017-07-23 12:08:53 +0300

    Update scripts/build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
-SCRIPTS_DIR=$1
-BCM_SDK=$2
-DST_DIR=$3
+BCM_SDK=$1
+DST_DIR=$2
 
 function print_usage
 {
-	echo "$0: Example: $0 <Scripts Dir> <BCM SDK Path> <Install Dir Path>"
-	echo "$0: Example: $0 ./scripts ~/dev/git/bcm_sdk /tmp/bcm_sdk_artifacts"
+	echo "$0: Example: $0  <BCM SDK Path> <Install Dir Path>"
+	echo "$0: Example: $0  ~/dev/git/bcm_sdk /tmp/bcm_sdk_artifacts"
 }
 
 ################################################################################
 # Check script user-supplied parameters
 #
-if [ -z $SCRIPTS_DIR ] ; then
-	echo "$0: Error: Provide scripts directory!"
-	print_usage
-	exit 1;
-fi
-
 if [ -z $BCM_SDK ] ; then
 	echo "$0: Error: Provide BCM SDK directory!"
 	print_usage
@@ -34,5 +27,5 @@ fi
 ################################################################################
 # Run the actual script
 #
-./$SCRIPTS_DIR/compile.sh $BCM_SDK
-./$SCRIPTS_DIR/copy_artifacts.sh $BCM_SDK $DST_DIR
+$BCM_SDK/scripts/compile.sh $BCM_SDK
+$BCM_SDK/scripts/copy_artifacts.sh $BCM_SDK $DST_DIR
